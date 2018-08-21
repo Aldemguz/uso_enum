d014a0b3d89651a4a6e13065ebe5ab2bcf5571d0
Aldemaro Guzman <aldemaroguzman93@gmail.com>
2018-08-21 14:06:38 -0430

    usando talla, creandole un constructor y metodos, no entendi del todo xD

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,41 @@ cuando se necesita que una variable no pueda tomar mas de un cierto grupo de val
 x ejem restringir los valores que pueda tomar un string.
 
 enum no puede declararse dentro de main
+los tipos enum no admiten la creacion de objetos, no se puede invocar a un constructor
+por eso se pone el "constructor" privado
 
  * @author aldemaro
  */
 
-import javax.swing.*;
+import java.util.*;
+//import javax.swing.*;
 public class Main {
 
     enum Talla {
-        pequeño,mediano,grande,muy_grade
+      MINI("S"),MEDIANO("M"),GRANDE("L"),MUY_GRANDE("XL");
+        
+        private Talla(String abreviatura){
+            this.abreviatura = abreviatura;
+        }
+        
+        public String dameAbreviatura(){
+            return abreviatura;
+        }
+        private String abreviatura;
     };
+    
     public static void main(String[] args) {
-      Talla s = Talla.pequeño;
-      Talla m = Talla.mediano;
-      Talla l = Talla.grande;
-      Talla xl = Talla.muy_grade;
-      
-      String x = JOptionPane.showInputDialog("Ingresa la talla");
-      if(s.toString().equalsIgnoreCase(x))
-            System.out.println("Talla correcta");
-      else
-            System.out.println("Talla incorrecta");
-          
+        
+        String entradaDatos;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Escribe una talla");
+        System.out.println("MINI,MEDIANO,GRANDE,MUY_GRANDE");
+        entradaDatos = entrada.nextLine().toUpperCase();
+                                      //tipo de clase;
+        Talla la_talla = Enum.valueOf(Talla.class, entradaDatos);
+        
+        System.out.println("Talla: "+la_talla);
+        System.out.println("Abreviatura: "+la_talla.dameAbreviatura());
     }
     
 }
